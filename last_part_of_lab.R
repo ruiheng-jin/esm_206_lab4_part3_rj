ddfe9e166756c0197f71551265f634894d61a060
ruiheng-jin <ruiheng@ucsb.edu>
2019-10-21 13:56:57 -0700

    added infant mortality graph

--- last_part_of_lab.R
+++ last_part_of_lab.R
@@ -6,4 +6,15 @@ library(here)
 
 # read in data
 db <- read_csv(here::here("data", "disease_burden.csv")) %>% 
-  janitor::clean_names()
+  janitor::clean_names() %>% 
+  rename(deaths_per_100k = death_rate_per_100_000)
+
+# create a subset, then graph
+db_sub <- db %>% 
+  filter(country_name %in% c("Afghanistan", "Japan", "United States", "Somalia")) %>% 
+  filter(age_group == "0-6 days", sex == "Both")
+
+# make basic graph
+ggplot(db_sub, aes(x = year,
+                   y = deaths_per_100k)) +
+  geom_line(aes(color = country_name))
